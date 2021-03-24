6122cfe4fe21e136f0c45e94058329b08fe288da
Saurabh Singh <76811463+SS-Croaker@users.noreply.github.com>
2021-03-24 11:08:38 +0530

    Update marketing_twitter_bot.py
    Adding favourite count as a parameter

--- marketing_twitter_bot.py
+++ marketing_twitter_bot.py
@@ -57,8 +57,8 @@ class RetweetListener(tweepy.StreamListener):
         
         # if not tweet.retweeted checks if my account has retweeted the tweet or not
         # if 'RT @' not in tweet.text check is the tweet is NOT a retweet
-        
-        if (not tweet.retweeted) and ('RT @' not in tweet.text):
+        favourites_count = tweet.favourites_count
+        if (not tweet.retweeted) and ('RT @' not in tweet.text) and (favourites_count > 100):
             
             try:
                 sleep_time = random.randrange(600,1200,60)
